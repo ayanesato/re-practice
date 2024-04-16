03827c74c8120a5d9994b2b62753e8c64d482e45
ayanesato <sato.ayane@genetec.co.jp>
2024-04-16 13:07:07 +0900

    test_driver.py 修正

--- test_driver.py
+++ test_driver.py
@@ -3,19 +3,20 @@ from isleapyear import is_leap_year
 import unittest
 class TestIsLeapYear(unittest.TestCase):
     def test_leap_year(self):
-        # うるう年の場合
-        self.assertEqual(is_leap_year(2000), True)
-        self.assertEqual(is_leap_year(2024), True)
-        self.assertEqual(is_leap_year(2020), True)
-
-    def test_non_leap_year(self):
-        # うるう年でない場合 同地分割
-        self.assertEqual(is_leap_year(1999), False)
-        self.assertEqual(is_leap_year(2001), False)
+        #4で割り切れるが100で割り切れない場合とその前後
         self.assertEqual(is_leap_year(2023), False)
+        self.assertEqual(is_leap_year(2024), True)
         self.assertEqual(is_leap_year(2025), False)
-        self.assertEqual(is_leap_year(2019), False)
-        self.assertEqual(is_leap_year(2021), False)
 
+        #4と100で割り切れる場合(平年)とその前後
+        self.assertEqual(is_leap_year(1899), False)
+        self.assertEqual(is_leap_year(1900), False)
+        self.assertEqual(is_leap_year(1901), False)
+
+        #4でも100でも400でも割り切れる場合とその前後
+        self.assertEqual(is_leap_year(1999), False)
+        self.assertEqual(is_leap_year(2000), True)
+        self.assertEqual(is_leap_year(2001), False)
+    
 if __name__ == '__main__':
     unittest.main()
