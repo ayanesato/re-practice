6d91635e79ab7127526a45325cff23be65c0c0de
ayanesato <sato.ayane@genetec.co.jp>
2024-04-18 16:47:32 +0900

    year%100 != 0が先、year%400 == 0があと

--- go/util/util.go
+++ go/util/util.go
@@ -3,9 +3,9 @@ package util
 func isLeapYear(year int) bool {
 	if year%4 != 0 {
 		return false
-	} else if year%400 == 0 {
-		return true
 	} else if year%100 != 0 {
+		return true
+	} else if year%400 == 0 {
 		return true
 	} else {
 		return false
