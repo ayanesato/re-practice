fe55c940b0a1f6845c3e9a7ea0df07440b2e1895
ayanesato <sato.ayane@genetec.co.jp>
2024-04-18 14:14:37 +0900

    and, orを使わないで書き直し

--- go/util/util.go
+++ go/util/util.go
@@ -1,6 +1,14 @@
 package util
 
 func isLeapYear(year int) bool {
-	return year%4 == 0 && (year%100 != 0 || year%400 == 0)
-
+	//return year%4 == 0 && (year%100 != 0 || year%400 == 0)
+	if year%4 == 0 {
+		if year%400 == 0 {
+			return true
+		}
+		if year%100 != 0 {
+			return true
+		}
+	}
+	return false
 }
