9d640193f3a0ba8d50e3b6aa39ad25d325f8af4b
ayanesato <sato.ayane@genetec.co.jp>
2024-04-18 14:58:50 +0900

    bool用の変数isLeapを追加、else ifを使うように修正

--- go/util/util.go
+++ go/util/util.go
@@ -2,13 +2,13 @@ package util
 
 func isLeapYear(year int) bool {
 	//return year%4 == 0 && (year%100 != 0 || year%400 == 0)
+	var isLeap bool
 	if year%4 == 0 {
 		if year%400 == 0 {
-			return true
-		}
-		if year%100 != 0 {
-			return true
+			isLeap = true
+		} else if year%100 != 0 {
+			isLeap = true
 		}
 	}
-	return false
+	return isLeap
 }
