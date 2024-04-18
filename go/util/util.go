01f9762e32b04448d9396526c92fe12919c744e5
ayanesato <sato.ayane@genetec.co.jp>
2024-04-18 16:45:01 +0900

    ifの処理順変更、isLeap削除、すぐにreturnする

--- go/util/util.go
+++ go/util/util.go
@@ -1,14 +1,13 @@
 package util
 
 func isLeapYear(year int) bool {
-	//return year%4 == 0 && (year%100 != 0 || year%400 == 0)
-	var isLeap bool
-	if year%4 == 0 {
-		if year%400 == 0 {
-			isLeap = true
-		} else if year%100 != 0 {
-			isLeap = true
-		}
+	if year%4 != 0 {
+		return false
+	} else if year%400 == 0 {
+		return true
+	} else if year%100 != 0 {
+		return true
+	} else {
+		return false
 	}
-	return isLeap
 }
